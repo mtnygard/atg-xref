13f7e072e387d66d9588b6fd8a60b4b90803857b
Michael Nygard <michael.nygard@n6consulting.com>
2011-01-15 14:01:26 -0600

    Glued fleet and compojure together

--- webui/project.clj
+++ webui/project.clj
@@ -2,6 +2,9 @@
   :description "Human interface for ATG codebase cross-reference."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.kjw/solrj "1.4.0"]]
+                 [org.clojars.kjw/solrj "1.4.0"]
+                 [fleet "0.9.4"]
+                 [compojure "0.5.3"]
+                 [ring/ring-jetty-adapter "0.3.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main webui.core)
--- webui/src/webui/core.clj
+++ webui/src/webui/core.clj
@@ -1,1 +1,21 @@
-(ns webui.core)
+(ns webui.core
+  (:use compojure.core
+        ring.adapter.jetty
+        compojure.response
+        [ring.util.response :only (response content-type)]
+        fleet)
+  (:require [compojure.route :as route])
+  (:gen-class))
+
+(fleet-ns views "templates")
+
+(extend-protocol Renderable
+  fleet.util.CljString
+  (render [this _] (response (.toString this))))
+
+(defroutes main-routes
+  (GET "/" [] (views/index))
+  (route/not-found "<h1>Page not found</h1>"))
+
+(defn -main [& args]
+  (run-jetty main-routes {:port 8080}))
