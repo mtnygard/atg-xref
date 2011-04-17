de2e19ec4470f429f3dc86dc4eb3814c21caba48
Michael Nygard <michael.nygard@n6consulting.com>
2011-04-17 14:47:30 -0500

    Decided to stop parsing Java. Will just use string-matching to crossreference components to classes. User already have Eclipse for code lookup & xref.

--- indexer/project.clj
+++ indexer/project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.kjw/solrj "1.4.0"]
-                 [org.antlr/antlr "3.2"]
-                 [com.habelitz/jsourceobjectizer "1.0.5"]]
+                 [org.antlr/antlr "3.2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [marginalia "0.5.0"]]
   :source-path "src/clojure"
