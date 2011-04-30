a502f6b38693e6971b177278d906750d7ee1b619
Michael Nygard <michael.nygard@n6consulting.com>
2011-04-30 15:12:46 -0500

    Index JSP files for start/end markers, ATG component references and page imports

--- indexer/src/atg/module.clj
+++ indexer/src/atg/module.clj
@@ -15,9 +15,11 @@
     (.load props (input-stream comp))
     props))
 
+(defn base [m] (:base m))
+
 (defn component-in-module
   [m sect compn]
-  (file-str (:base m) "/" sect compn ".properties"))
+  (file-str (base m) "/" sect compn ".properties"))
 
 (defn cleanse
   [s pats]
@@ -37,16 +39,40 @@
     (merge (name-to-keyword (java-component-properties path))
            (struct component sect compn body path (component-names-in body)))))
 
-(defn suffix-p [suffix f] (.endsWith (.getPath f) suffix))
-(def properties? (partial suffix-p ".properties"))
-(def java? (partial suffix-p ".java"))
+(defn suffix? [f suffix] (.endsWith (.getPath f) suffix))
+(defn properties? [f]  (suffix? f ".properties"))
+(defn java? [f] (suffix? f ".java"))
+(defn jsp? [f] (or (suffix? f ".jsp") (suffix? f ".jspf")))
 
-(defn component-files [m sub] (filter properties? (file-seq (file-str (:base m) "/" sub))))
-(defn source-files [m] (filter java? (file-seq (:base m))))
+(defn file-type-seq [fn dir] (filter fn (file-seq dir)))
+
+(defn component-files [m sub] (file-type-seq properties? (file-str (base m) "/" sub)))
+(defn source-files [m] (file-type-seq java? (base m)))
+
+(defn make-jsp [name file] (list name file))
+(defn jsp-file [j] (second j))
+(defn jsp-name [j] (first j))
+
+(defn make-web-module [name base] (list name base))
+(defn web-module-name [webm] (first webm))
+(defn web-module-base [webm] (second webm))
+(defn make-jsp-in-module [webm jspf] (make-jsp (str/replace jspf (str (web-module-base webm)) "") jspf))
+(defn web-module-jsps [webm]
+  (map #(make-jsp-in-module webm %) (file-type-seq jsp? (web-module-base webm))))
+
+(defn make-web-module-in-module [m webmodn] (make-web-module webmodn (file-str (base m) "/j2ee/" webmodn)))
+(defn web-modules [m]
+  (if-let [webmods (:ATG-Web-Module m)] 
+    (for [modn (str/split webmods #" ")]
+      (make-web-module-in-module m (str/replace modn #"^.*/" "")))
+    '()
+    ))
+
+(defn jsp-files [m] (file-type-seq jsp? (file-str (base m) "/"  )))
 
 (defn- canonicalize
   [m prefix compf]
-  (let [compn (.replaceFirst (.getCanonicalPath compf) (str (.getCanonicalPath (:base m)) "/" prefix) "")
+  (let [compn (.replaceFirst (.getCanonicalPath compf) (str (.getCanonicalPath (base m)) "/" prefix) "")
         compn (.replace compn ".properties" "")]
     compn))
 
@@ -65,7 +91,7 @@
   (Manifest. (input-stream (manifest-file dir))))
 
 (defn parse-manifest
-  [base dir]
+  [dir]
   "Locate and parse the manifest file for a module with the given directory and qualified name"
   (assoc
    (name-to-keyword (.. (java-manifest dir) getMainAttributes))
@@ -83,4 +109,4 @@
 
 (defn load-modules
   [root]
-  (map #(parse-manifest root %) (module-directories root)))
+  (map #(parse-manifest %) (module-directories root)))
---
+++ indexer/src/indexer/jsp.clj
@@ -0,0 +1,39 @@
+(ns indexer.jsp
+  "Simplistic parser for JSP files. Does not validate syntax, may get extremely confused at times."
+  (:use atg.module
+        [clojure.set :only (union)])
+  (:require [clojure.string :as str]))
+
+(defn- re-extract
+  [re body]
+  (set (map second (re-seq re body))))
+
+(defn page-imports
+  [body]
+  (re-extract #"<%@\s*page\s+import\s*=\s*\"(.*)\"" body))
+
+(defn start-code-tag
+  [body]
+  (re-extract #"<!--\s*(B:...)\s*-->" body))
+
+(defn end-code-tag
+  [body]
+  (re-extract #"<!--\s*(E:...)\s*-->" body))
+
+(defn taglibs
+  [body]
+  (re-extract #"<%@\s*taglib\s+uri=\"([^\"]*)\"" body))
+
+(defn bean-references
+  [body]
+  (union
+   (re-extract #"dsp:importbean\s+bean=\"(.*)\"" body)
+   (re-extract #"dsp:droplet\s+name=\"(/[^\"]*)\"" body)))
+
+(defstruct jsp-info :name :body :page-imports :start-code-tag :end-code-tag :taglibs :references)
+
+(defn parse-jsp
+  ([name body]
+     (struct jsp-info name body (page-imports body) (start-code-tag body) (end-code-tag body) (taglibs body) (bean-references body)))
+  ([module web-module jsp]
+     (parse-jsp (jsp-name jsp) (slurp (jsp-file jsp)))))
