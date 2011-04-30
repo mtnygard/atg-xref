5f9a60eb78735b9b582adb107a34d5a0af6946c7
Michael Nygard <michael.nygard@n6consulting.com>
2011-04-30 13:33:19 -0500

    Remove marginalia from projects. It's currently broken.

--- indexer/project.clj
+++ indexer/project.clj
@@ -5,8 +5,6 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.kjw/solrj "1.4.0"]
                  [org.antlr/antlr "3.2"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [marginalia "0.5.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :main indexer.core
-  :tasks [marginalia.tasks]
 )
